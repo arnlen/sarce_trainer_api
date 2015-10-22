2413d60ad339f8caf567fa07dcb7f22697c3e114
Arnaud Lenglet <arlenglet@gmail.com>
2015-10-22 14:17:05 +0200

    config(ams): use ActiveModel::Serializer::Adapter::JsonApi as JSON Serializer

--- config/initializers/ams_json_adapter.rb
+++ config/initializers/ams_json_adapter.rb
@@ -1,4 +1,5 @@
 # To play nice with EmberJS Data
 # Following recommendations from this guide:
 # http://wyeworks.com/blog/2015/6/30/how-to-build-a-rails-5-api-only-and-ember-application
-ActiveModel::Serializer.config.adapter = :json
+# ActiveModel::Serializer.config.adapter = :json
+ActiveModel::Serializer.config.adapter = ActiveModel::Serializer::Adapter::JsonApi
